14d4537cb2a403d12453e047bf90bf9b3759cac9
koyasuk <koyasuk@lafayette.edu>
2023-04-23 21:45:44 -0400

    finished the back-end will do css later

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const access_key = "c16cd0d608ce7d0c8302c50430918068";
 const form = document.querySelector('form');
 const locationInput = document.querySelector('input[type=text]');
+const locationElement = document.querySelector('.location');
 const temperatureElement = document.querySelector('.degree');
 const descriptionElement = document.querySelector('.forecast');
-const locationElement = document.querySelector('.location');
+const weatherInfo = document.querySelector('.weather-info');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
   const location = locationInput.value;
   const url = `http://api.weatherstack.com/current?access_key=${access_key}&query=${location}`;
 
@@ -16,10 +17,10 @@ form.addEventListener('submit', (e) => {
       const temperature = data.current.temperature;
       const description = data.current.weather_descriptions[0];
       const windSpeed = data.current.wind_speed;
-      temperatureElement.textContent = `${temperature}°C`;
-      descriptionElement.textContent = description;
-      locationElement.textContent = `Current weather in ${location}:`;
-      console.log(temperature);
+      locationElement.innerHTML = `Current weather in ${location}:`;
+      temperatureElement.innerHTML = `${temperature}°C`;
+      descriptionElement.innerHTML = description;
+      weatherInfo.innerHTML = `Wind speed: ${windSpeed} km/h`;
     })
     .catch(error => console.log('error', error));
 });
